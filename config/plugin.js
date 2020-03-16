6149c4807e45319d5650732d485ece250ddee727
Erzhuyijian <1821344102@qq.com>
2020-03-16 20:47:56 +0800

    egg.js 插件导入

--- config/plugin.js
+++ config/plugin.js
@@ -1,9 +1,35 @@
+/*
+ * @Author: Wenzhe
+ * @Date: 2020-03-16 16:28:48
+ * @LastEditors: Wenzhe
+ * @LastEditTime: 2020-03-16 19:36:21
+ */
 'use strict';
 
 /** @type Egg.EggPlugin */
-module.exports = {
-  // had enabled by egg
-  // static: {
-  //   enable: true,
-  // }
+// module.exports = {
+//   // had enabled by egg
+//   // static: {
+//   //   enable: true,
+//   // }
+// };
+
+exports.mongoose = {
+  enable: true,
+  package: 'egg-mongoose',
+};
+
+exports.validate = {
+  enable: true,
+  package: 'egg-validate',
+};
+
+exports.jwt = {
+  enable: true,
+  package: 'egg-jwt',
+};
+
+exports.cors = {
+  enable: true,
+  package: 'egg-cors',
 };

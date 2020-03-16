f4f35a82242378a8aa0fff4e50f636a22fed3dc7
Erzhuyijian <1821344102@qq.com>
2020-03-16 20:57:29 +0800

    在 router.js 中添加 添加用户的路由，路径为 /api/user 方法为 POST

--- app/router.js
+++ app/router.js
@@ -1,3 +1,9 @@
+/*
+ * @Author: Wenzhe
+ * @Date: 2020-03-16 16:28:48
+ * @LastEditors: Wenzhe
+ * @LastEditTime: 2020-03-16 19:28:27
+ */
 'use strict';
 
 /**
@@ -6,4 +12,7 @@
 module.exports = app => {
   const { router, controller } = app;
   router.get('/', controller.home.index);
+
+  // 用户路由
+  router.post('/api/user', controller.user.createUser);
 };

2abedf9066733c67904c19400c3f120434ecd2de
Erzhuyijian <1821344102@qq.com>
2020-03-17 23:08:52 +0800

    提取公用方法通过name查询用户, findByName

--- app/service/user.js
+++ app/service/user.js
@@ -2,7 +2,7 @@
  * @Author: Wenzhe
  * @Date: 2020-03-16 18:53:20
  * @LastEditors: Wenzhe
- * @LastEditTime: 2020-03-17 14:02:44
+ * @LastEditTime: 2020-03-17 18:40:01
  */
 'use strict';
 
@@ -13,7 +13,7 @@ class UserService extends Service {
   async create(payload) {
     const { ctx } = this;
     // 对于 name 做校验
-    const doc = await ctx.model.User.findOne({ name: payload.name });
+    const doc = await ctx.service.user.findByName(payload.name);
     if (doc) {
       ctx.throw(400, '用户名已被占用');
     }
@@ -47,6 +47,12 @@ class UserService extends Service {
     return ctx.model.User.findByIdAndUpdate(_id, payload);
   }
 
+  // comm ------------------------------------
+  // 通过name查询用户
+  async findByName(name) {
+    const { ctx } = this;
+    return ctx.model.User.findOne({ name });
+  }
   // 通过id查询用户
   async findById(_id) {
     const { ctx } = this;

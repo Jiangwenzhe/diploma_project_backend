cc4d8f804eed7edd995dd43b09262dd1a4d77674
Erzhuyijian <1821344102@qq.com>
2020-03-17 14:03:22 +0800

    移除 用户 service 中的 todo ,  添加校验

--- app/service/user.js
+++ app/service/user.js
@@ -2,14 +2,14 @@
  * @Author: Wenzhe
  * @Date: 2020-03-16 18:53:20
  * @LastEditors: Wenzhe
- * @LastEditTime: 2020-03-17 12:37:03
+ * @LastEditTime: 2020-03-17 14:00:01
  */
 'use strict';
 
 const Service = require('egg').Service;
 
 class UserService extends Service {
-  // TODO: 创建用户 需要避免重复创建
+  // 创建用户
   async create(payload) {
     const { ctx } = this;
     // 对于 name 做校验
@@ -20,32 +20,37 @@ class UserService extends Service {
     return ctx.model.User.create(payload);
   }
 
-  // TODO: 删除用户
+  // 删除用户
   async destroy(_id) {
     const { ctx } = this;
-    const user = await ctx.service.user.find(_id);
+    const user = await ctx.service.user.findById(_id);
     if (!user) {
       ctx.throw(404, 'user not found');
     }
     return ctx.model.User.findByIdAndRemove(_id);
   }
 
-  // TODO: 更新用户
+  // 更新用户
   async update(_id, payload) {
+    console.log('--updaye', _id, payload);
     const { ctx } = this;
-    const user = await ctx.service.user.find(_id);
+    const user = await ctx.service.user.findById(_id);
     if (!user) {
       ctx.throw(404, 'user not found');
+    }
+    // 对于 name 做校验
+    if (user.name !== payload.name) {
+      const doc = await ctx.model.User.findOne({ name: payload.name });
+      if (doc) {
+        ctx.throw(400, '用户名已被占用');
+      }
     }
     return ctx.model.User.findByIdAndUpdate(_id, payload);
   }
 
-  // TODO: 通过id查询用户
-  async find(_id) {
+  // 通过id查询用户
+  async findById(_id) {
     const { ctx } = this;
-    if (!_id.match(/^[0-9a-fA-F]{24}$/)) {
-      ctx.throw(400, 'id参数错误');
-    }
     return ctx.model.User.findById(_id);
   }
 }

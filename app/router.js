ac0475b4bad5395a6212492f974ee5b0cbb839e7
Erzhuyijian <1821344102@qq.com>
2020-03-17 14:01:50 +0800

    更新router 添加 delete user/:Id, get user/:id, put user:/id 的路由

--- app/router.js
+++ app/router.js
@@ -2,7 +2,7 @@
  * @Author: Wenzhe
  * @Date: 2020-03-16 16:28:48
  * @LastEditors: Wenzhe
- * @LastEditTime: 2020-03-16 19:28:27
+ * @LastEditTime: 2020-03-17 14:00:20
  */
 'use strict';
 
@@ -13,6 +13,9 @@ module.exports = app => {
   const { router, controller } = app;
   router.get('/', controller.home.index);
 
-  // 用户路由
+  // 用户 user 操作相关路由 =============================
   router.post('/api/user', controller.user.createUser);
+  router.delete('/api/user/:id', controller.user.destroyUser);
+  router.get('/api/user/:id', controller.user.findSingleUserById);
+  router.put('/api/user/:id', controller.user.updateUser);
 };

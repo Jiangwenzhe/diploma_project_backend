f4e9e5f469cf25a3c627e9e0596dc6b788e49470
Erzhuyijian <1821344102@qq.com>
2020-03-17 12:59:53 +0800

    model 中的用户 添加唯一性条件

--- app/model/user.js
+++ app/model/user.js
@@ -2,14 +2,14 @@
  * @Author: Wenzhe
  * @Date: 2020-03-16 16:54:00
  * @LastEditors: Wenzhe
- * @LastEditTime: 2020-03-16 19:13:54
+ * @LastEditTime: 2020-03-17 12:02:41
  */
 'use strict';
 
 module.exports = app => {
   const mongoose = app.mongoose;
   const UserSchema = new mongoose.Schema({
-    name: { type: String, required: true },
+    name: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     description: { type: String },
     createdAt: { type: Date, default: Date.now },

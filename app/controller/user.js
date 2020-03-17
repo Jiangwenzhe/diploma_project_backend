0d5cb9850e5d3cb20ffc2f0d41350d1906779985
Erzhuyijian <1821344102@qq.com>
2020-03-17 14:04:20 +0800

    添加 删除用户， 修改用户， 根据单个id查询用户的 controller

--- app/controller/user.js
+++ app/controller/user.js
@@ -2,7 +2,7 @@
  * @Author: Wenzhe
  * @Date: 2020-03-16 19:11:42
  * @LastEditors: Wenzhe
- * @LastEditTime: 2020-03-16 19:47:45
+ * @LastEditTime: 2020-03-17 13:59:49
  */
 'use strict';
 
@@ -26,6 +26,40 @@ class UserController extends Controller {
     const res = await service.user.create(payload);
     // 设置响应内容和响应状态码
     ctx.helper.success({ ctx, res });
+  }
+  // 删除用户
+  async destroyUser() {
+    const { ctx, service } = this;
+    const { id } = ctx.params;
+    if (!id.match(/^[0-9a-fA-F]{24}$/)) {
+      ctx.throw(400, 'id参数错误');
+    }
+    await service.user.destroy(id);
+    // 设置响应内容和响应状态码
+    ctx.helper.success({ ctx });
+  }
+  // 修改用户
+  async updateUser() {
+    const { ctx, service } = this;
+    ctx.validate(UserRule, ctx.request.body);
+    const { id } = ctx.params;
+    if (!id.match(/^[0-9a-fA-F]{24}$/)) {
+      ctx.throw(400, 'id参数错误');
+    }
+    const payload = ctx.request.body || {};
+    const res = await service.user.update(id, payload);
+    ctx.helper.success({ ctx, res });
+  }
+  // 根据id查询单个用户
+  async findSingleUserById() {
+    const { ctx, service } = this;
+    const { id } = ctx.params;
+    if (!id.match(/^[0-9a-fA-F]{24}$/)) {
+      ctx.throw(400, 'id参数错误');
+    }
+    const res = await service.user.findById(id);
+    const msg = res === null ? '没有该用户' : null;
+    ctx.helper.success({ ctx, res, msg });
   }
 }
 

69aae87306089437155b68792c4cc93207d5cff4
Erzhuyijian <1821344102@qq.com>
2020-03-17 13:01:28 +0800

    添加查询用户与添加用户的约束

--- app/service/user.js
+++ app/service/user.js
@@ -2,7 +2,7 @@
  * @Author: Wenzhe
  * @Date: 2020-03-16 18:53:20
  * @LastEditors: Wenzhe
- * @LastEditTime: 2020-03-16 21:57:07
+ * @LastEditTime: 2020-03-17 12:37:03
  */
 'use strict';
 
@@ -12,6 +12,11 @@ class UserService extends Service {
   // TODO: 创建用户 需要避免重复创建
   async create(payload) {
     const { ctx } = this;
+    // 对于 name 做校验
+    const doc = await ctx.model.User.findOne({ name: payload.name });
+    if (doc) {
+      ctx.throw(400, '用户名已被占用');
+    }
     return ctx.model.User.create(payload);
   }
 
@@ -35,9 +40,12 @@ class UserService extends Service {
     return ctx.model.User.findByIdAndUpdate(_id, payload);
   }
 
-  // TODO: 查询用户
+  // TODO: 通过id查询用户
   async find(_id) {
     const { ctx } = this;
+    if (!_id.match(/^[0-9a-fA-F]{24}$/)) {
+      ctx.throw(400, 'id参数错误');
+    }
     return ctx.model.User.findById(_id);
   }
 }

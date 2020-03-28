c86a9a27ce94e5b9d6d46bb1b5481eb9b7532eaf
Erzhuyijian <1821344102@qq.com>
2020-03-28 21:00:02 +0800

    home: controller:  更改 home -> index() 函数的返回值，使其更语义化

--- app/controller/home.js
+++ app/controller/home.js
@@ -2,16 +2,22 @@
  * @Author: Wenzhe
  * @Date: 2020-03-16 16:28:48
  * @LastEditors: Wenzhe
- * @LastEditTime: 2020-03-16 16:33:42
+ * @LastEditTime: 2020-03-28 20:46:37
  */
 'use strict';
 
 const Controller = require('egg').Controller;
+const moment = require('moment');
 
 class HomeController extends Controller {
   async index() {
     const { ctx } = this;
-    ctx.body = 'diploma_project_backend is running...';
+    const res = {
+      app_status: 'running',
+      current_time: moment().format('YYYY-MM-DD, HH:mm:ss'),
+    };
+    // 设置响应内容和响应状态码
+    ctx.helper.success({ ctx, res });
   }
 }
 

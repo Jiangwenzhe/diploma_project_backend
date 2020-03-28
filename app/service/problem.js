cdeadc3df8cc5027f8f0d49efb38f541dfffc267
Erzhuyijian <1821344102@qq.com>
2020-03-28 14:45:13 +0800

    problem: service:  添加可以分页获取题目的 index 函数

--- app/service/problem.js
+++ app/service/problem.js
@@ -2,7 +2,7 @@
  * @Author: Wenzhe
  * @Date: 2020-03-26 14:55:02
  * @LastEditors: Wenzhe
- * @LastEditTime: 2020-03-26 18:19:46
+ * @LastEditTime: 2020-03-28 14:40:34
  */
 
 'use strict';
@@ -18,6 +18,7 @@ class ProblemsService extends Service {
     const newPayload = { ...payload, pid: CreatedID.id };
     try {
       const res = ctx.model.Problem.create(newPayload);
+      // TODO: 后期可能需要加入写入 testcase 的活
       return res;
     } catch (e) {
       ctx.throw(400, e);
@@ -25,6 +26,22 @@ class ProblemsService extends Service {
   }
 
   // TODO: 获取所有题目，需要支持 antd 分页
+  async index(payload) {
+    // TODO: 需要添加关键字模糊搜索 注意参考 leetcode 的实现
+    const { current, pageSize } = payload;
+    const { ctx } = this;
+    let res = [];
+    let total = 0;
+    // 计算skip
+    const skip = ((Number(current)) - 1) * Number(pageSize || 10);
+    // 索取所有题目的数量
+    total = await ctx.model.Problem.count({}).exec();
+    res = await this.ctx.model.Problem.find({}, { title: 1, tags: 1, submit: 1, solve: 1 }).skip(skip).limit(Number(pageSize))
+      .sort({ createdAt: -1 })
+      .exec();
+    return { total, list: res, pageSize, current };
+  }
+
 }
 
 module.exports = ProblemsService;

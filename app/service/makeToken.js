88fcc0d5e7fe37b8dc49ba02c4aea880e981f31f
Erzhuyijian <1821344102@qq.com>
2020-04-02 17:32:43 +0800

    makeToken: service: 在 jwt 保存的数据中加入 privilege 权限字段与 name 名称字段

--- app/service/makeToken.js
+++ app/service/makeToken.js
@@ -2,19 +2,21 @@
  * @Author: Wenzhe
  * @Date: 2020-03-17 18:50:00
  * @LastEditors: Wenzhe
- * @LastEditTime: 2020-03-17 23:06:42
+ * @LastEditTime: 2020-04-02 16:08:27
  */
 'use strict';
 
 const Service = require('egg').Service;
 
 class MakeTokenService extends Service {
-  async apply(_id) {
+  async apply(_id, privilege, name) {
     const { ctx } = this;
 
     return ctx.app.jwt.sign({
       data: {
         _id,
+        name,
+        privilege,
       },
       exp: Math.floor(Date.now() / 1000) + (60 * 60 * 24 * 7),
     }, ctx.app.config.jwt.secret);

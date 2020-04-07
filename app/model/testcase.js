39ad9377cce8f8cd537c6530bbbf526e23c3b2af
Erzhuyijian <1821344102@qq.com>
2020-04-07 12:02:41 +0800

    testcase: model: 添加字段 path: 用来表示上传文件在系统中的路径

--- app/model/testcase.js
+++ app/model/testcase.js
@@ -2,7 +2,7 @@
  * @Author: Wenzhe
  * @Date: 2020-04-05 08:45:54
  * @LastEditors: Wenzhe
- * @LastEditTime: 2020-04-05 09:39:31
+ * @LastEditTime: 2020-04-07 11:37:49
  */
 'use strict';
 
@@ -14,6 +14,7 @@ module.exports = app => {
     },
     info: Array,
     spj: Boolean,
+    path: String,
     createdAt: {
       type: Date,
       default: Date.now,

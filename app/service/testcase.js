d774ab17659a673c1e153c25670a8947a8442bbc
Erzhuyijian <1821344102@qq.com>
2020-04-07 12:06:18 +0800

    testcase: service: 与执行用例上传接口通信

--- app/service/testcase.js
+++ app/service/testcase.js
@@ -2,24 +2,39 @@
  * @Author: Wenzhe
  * @Date: 2020-04-04 17:49:06
  * @LastEditors: Wenzhe
- * @LastEditTime: 2020-04-05 10:12:34
+ * @LastEditTime: 2020-04-07 11:54:45
  */
 'use strict';
 
 const Service = require('egg').Service;
+const request = require('superagent');
+
 
 class TestcaseService extends Service {
   async upload(payload) {
-    console.log('------------service file payload', payload);
+    const { service, config, ctx } = this;
+    const { path } = payload;
+    const res = await request.post(config.TestCaseUploadServer)
+      .type('form')
+      .field('spj', false)
+      .attach('file', path);
+    const data = JSON.parse(res.text).data;
+    const result = await service.testcase.create({
+      ...data,
+      path,
+    });
+    ctx.logger.info('从执行用例上传服务器中返回的数据: %j', result);
+    return result;
   }
 
   async create(payload) {
     const { ctx } = this;
-    const { id, spj, info } = payload;
+    const { id, spj, info, path } = payload;
     const custom_payload = {
       test_case_id: id,
       spj,
       info,
+      path,
     };
     try {
       const res = await ctx.model.Testcase.create(custom_payload);

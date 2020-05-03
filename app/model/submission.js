ed926498795ae5f36c678a7cfc1ba85253ba3be3
Erzhuyijian <1821344102@qq.com>
2020-05-03 12:53:24 +0800

    submission / model 添加 status 参数，用来区别是练习还是比赛

--- app/model/submission.js
+++ app/model/submission.js
@@ -2,7 +2,7 @@
  * @Author: Wenzhe
  * @Date: 2020-04-13 08:33:47
  * @LastEditors: Wenzhe
- * @LastEditTime: 2020-04-13 16:12:05
+ * @LastEditTime: 2020-05-03 09:53:10
  */
 'use strict';
 
@@ -33,6 +33,11 @@ module.exports = app => {
     status_info: Object,
     content: {
       type: String,
+    },
+    // status 是 1，代表 practice, 2 代表 contest
+    status: {
+      type: Number,
+      default: 1,
     },
   });
   return mongoose.model('Submission', SubmissionSchema);
